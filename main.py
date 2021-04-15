ca33378cbe5b5e43fa3c1f05644e1a5c99322530
Ethan <>
2021-04-15 21:11:07 +0000

    Deleted something

--- main.py
+++ main.py
@@ -44,10 +44,7 @@ def delete_encouragment(index):
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
+
 
   msg = message.content
 
